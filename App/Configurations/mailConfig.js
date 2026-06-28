6f5b5435d91c9f560db962b7dc13cac5291ad11e
Shivam-Software-Engineer <svmpathak1@gmail.com>
2026-06-28 23:35:13 +0530

    Config

--- App/Configurations/mailConfig.js
+++ App/Configurations/mailConfig.js
@@ -3,8 +3,9 @@ const nodemailer = require("nodemailer");
 // Create a transporter using SMTP
 const transporter = nodemailer.createTransport({
   host: "smtp.gmail.com",
-  port: 465,
-  secure: true, // use STARTTLS (upgrade connection to TLS after connecting)
+ port: 587,
+  secure: false,
+  family: 4, // use STARTTLS (upgrade connection to TLS after connecting)
   auth: {
     user: process.env.SMTP_USER,
     pass: process.env.SMTP_PASS,
